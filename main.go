b7fd4a00a79a4a395fd358b7a01c1391bc532d41
Adam Houston <phazyy9@gmail.com>
2020-04-26 15:08:53 +0100

    Printing out results from similar deployments

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/ahstn/karetaker/pkg/kubernetes"
@@ -78,5 +80,32 @@ func main() {
 			fmt.Printf("Selected %s in Namespace %s for %s.\n", answers.Resources, answers.Namespace, answers.Age)
 		})
 
+	commando.
+		Register("duplicate").
+		SetDescription("Execute a batch run using pre-existing clean-up logic").
+		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+			namespace := "default"
+			appLabel := "app=nginx"
+			instanceLabel := "version"
+			clientset, err := kubernetes.Config("")
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			deployments, err := kubernetes.ListDuplicateDeployments(clientset, namespace, appLabel, instanceLabel)
+			for _, deployment := range deployments {
+				fmt.Printf("deploy/%s\n", deployment)
+			}
+
+			w := new(tabwriter.Writer)
+			w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+			defer w.Flush()
+
+			fmt.Fprintf(w, "%s\t%s\n", "DEPLOYMENT", "MATCHES")
+			for deployment, matches := range deployments {
+				fmt.Fprintf(w, "%s\t%v\t\n", deployment, matches)
+			}
+		})
 	commando.Parse(nil)
 }
