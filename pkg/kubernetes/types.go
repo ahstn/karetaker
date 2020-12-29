55a12018f7f8f06d3b8ff72006f99b1b63f30f31
Adam Houston <phazyy9@gmail.com>
2020-12-29 13:42:49 +0000

    Adding static types for different resources

---
+++ pkg/kubernetes/types.go
@@ -0,0 +1,11 @@
+package kubernetes
+
+import "k8s.io/apimachinery/pkg/runtime/schema"
+
+var (
+	PodSchema = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
+	ConfigMapSchema = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "configmaps"}
+	SecretSchema = schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"}
+
+	DeploymentSchema = schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "deployments"}
+)
